d786f574cdf6aea35bc8d018e07d004486ed0067
xerycks <xerycks@gmail.com>
2020-04-27 19:52:20 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,66 +2,95 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const loda = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
-var posts = [];
+const postSchema = new mongoose.Schema({
+  blogTitle: String,
+  blogContent: String,
+});
 
-app.set('view engine', 'ejs');
+const Post = mongoose.model("Post", postSchema);
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-
-app.get('/', (req, res) => res.render('home', {
-  homeStartingContent: homeStartingContent,
-  posts: posts
-}))
-
-app.get('/about', (req, res) => res.render('about', {
-  aboutContent: aboutContent,
-}))
-
-app.get('/contact', (req, res) => res.render('contact', {
-  contactContent: contactContent,
-}))
-
-app.get('/compose', function (req, res) {
-  res.render('compose');
-})
-
-app.get('/post', function (req, res) {
-  res.render('post');
-})
-
-app.get('/posts/:postTitle', function (req, res) {
-  for (var i = 0; i < posts.length; i++) {
-    if (loda.lowerCase(req.params.postTitle) === loda.lowerCase(posts[i].title)) {
-      var pN = i;
-
-      res.render('post', {
-        heading: posts[pN].title,
-        para: posts[pN].content
-      })
+app.get("/", (req, res) => {
+  Post.find({}, function (err, result) {
+    if (!err) {
+      if (result) {
+        res.render("home", {
+          posts: result,
+        });
+      }
     }
-  }
+  });
+});
 
-})
+app.get("/about", (req, res) =>
+  res.render("about", {
+    aboutContent: "You'll Know Soon",
+  })
+);
 
-app.post('/compose', function (req, res) {
-  const post = {
-    title: req.body.title,
-    content: req.body.content
-  }
-  posts.push(post)
-  res.redirect('/');
-})
+app.get("/contact", (req, res) =>
+  res.render("contact", {
+    contactContent: "Nothing Yet",
+  })
+);
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    blogTitle: req.body.title,
+    blogContent: req.body.content,
+  });
+
+  post.save("", function (err) {
+    if (!err) {
+      res.redirect("/");
+    } else {
+      res.send("ERROR 69");
+    }
+  });
+});
+
+app.get("/post", function (req, res) {
+  res.render("post");
+});
+
+app.get("/posts/:postTitle", function (req, res) {
+  Post.findOne(
+    {
+      blogTitle: req.params.postTitle,
+    },
+    function (err, result) {
+      if (!err) {
+        if (result) {
+          res.render("post", {
+            heading: result.blogTitle,
+            para: result.blogContent,
+          });
+        }
+      }
+    }
+  );
+});
 
 app.listen(process.env.PORT || 5000, function () {
   console.log("Server started on port 3000");
